e327681935d43a81afc6befda77f7653fca4f84c
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-01-17 02:31:02 +0100

    changed if to ternary

--- src/App.js
+++ src/App.js
@@ -19,13 +19,7 @@ function App() {
   };
 
   const toogleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-
-      return task;
-    }));
+    setTasks(tasks => tasks.map(task => task.id === id ? { ...task, done: !task.done } : task));
   };
 
   const setAllDone = () => {
