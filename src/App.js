92a642485d1be018064b3b66803e7ee41f462f6c
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-02-21 22:25:31 +0100

    added navigation to the website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import TaskPage from "./features/tasks/TaskPage";
+import TasksPage from "./features/tasks/TasksPage";
+import { StyledNavLink, StyledNavItem, StyledNavList } from "./styled";
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <StyledNavList>
+                <StyledNavItem>
+                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+                </StyledNavItem>
+                <StyledNavItem>
+                    <StyledNavLink to="/autor">O autorze</StyledNavLink>
+                </StyledNavItem>
+            </StyledNavList>
+            <Switch>
+                <Route path={"/zadania/:id"}>
+                    <TaskPage />
+                </Route>
+                <Route path={"/zadania"}>
+                    <TasksPage />
+                </Route>
+                <Route path={"/autor"}>
+                    <AuthorPage />
+                </Route>
+                <Route path={"/"}>
+                    <Redirect to={"/zadania"} />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
