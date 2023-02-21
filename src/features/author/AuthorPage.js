1e9e8d99522b6650ef2329ea773d36bb3f4becf2
Bartlomiej Snochowski <bartlomiej.snochowski@gmail.com>
2023-02-22 00:12:18 +0100

    author page update

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -8,10 +8,10 @@ export default () => (
         <Section
             title="Bartłomiej Snochowski"
             body={
-                <p>Mieszkam na obrzeżach Kielc. Mam 24 lata i jestem
+                <p>Mieszkam na obrzeżach <strong>Kielc</strong>. Mam <strong>24</strong> lata i jestem
                 studentem ostatniego semestru automatyki. Kurs traktuję
-                jako coś poważnego, a sama wizja pracy w IT bardzo mnie interesuje i napędza
-                do działania. Mam nadzieję, że wiele się jeszcze nauczę i koniec końców uda mi
+                jako coś poważnego, a sama wizja <strong>pracy w IT</strong> bardzo mnie interesuje i napędza
+                do działania. Mam nadzieję, że wiele się jeszcze nauczę i koniec końców <strong>uda</strong> mi
                 się pracować jako programista :)
             </p>
             }
